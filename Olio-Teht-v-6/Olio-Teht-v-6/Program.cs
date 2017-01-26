b7f2ed7d0c226837a481f0695fd2856554e6ae9c
h8324 <h8324@ICT-421-WS-14>
2017-01-26 20:41:37 +0200

    initial commit

---
+++ Olio-Teht-v-6/Olio-Teht-v-6/Program.cs
@@ -0,0 +1,19 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Olio_Teht_v_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Anna matka kilometreinä");
+            int satanen matka / 
+            int kulutus 7.02 / satanen
+        }
+    }
+}
