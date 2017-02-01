2ef7a14d850b8218a36b67bf516a5022c3542e00
h8324 <h8324@ICT-421-WS-14>
2017-02-01 17:27:45 +0200

    Final commit

--- Olio-Teht-v-6/Olio-Teht-v-6/Program.cs
+++ Olio-Teht-v-6/Olio-Teht-v-6/Program.cs
@@ -7,13 +7,31 @@ using System.Threading.Tasks;
 
 namespace Olio_Teht_v_6
 {
+    /// <summary>
+    /// Laskee polttoaineen kulutuksen.
+    /// </summary>
     class Program
     {
         static void Main(string[] args)
         {
-            Console.Write("Anna matka kilometreinä");
-            int satanen matka / 
-            int kulutus 7.02 / satanen
+            double kulutus = 7.02;
+            double hinta = 1.595;
+            Console.Write("Anna matka kilometreinä ");
+            string line = Console.ReadLine();
+            double matka;
+            bool result = Double.TryParse(line, out matka);
+            if (result)
+            {
+                double bensiini = matka / 100 * kulutus;
+                double kustannukset = bensiini * hinta;
+                Console.WriteLine("Bensiinin kulutus on {0:F2} litraa.", bensiini);
+                Console.WriteLine("Kustannukset ovat {0:F2} euroa.", kustannukset);
+            }
+            else
+            {
+                Console.WriteLine("Et antanut lukua!");
+            }
+            Console.ReadLine();
         }
     }
 }
